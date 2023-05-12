84843dffa3871fb16ceafd1c69d2d2e8c8550e4d
JungMinHo <sucessmh25@naver.com>
2023-05-12 22:33:57 +0900

    Create SQL26_or.sql

---
+++ SQL26_or.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE 1=1
+AND NAME='Ella' OR NAME='Lucy' OR NAME='Pickle' OR NAME='Rogan' OR NAME='Sabrina' OR NAME='Mitty'
